84f77e5a6ae79f19f71087039640c28970061ea3
Maximilian-hrmn <Maxi.herman@t-online.de>
2025-05-08 22:30:05 +0200

    server klasse gebaut

---
+++ server.py
@@ -0,0 +1,33 @@
+import socket
+
+class Server:
+    def __init__(self, ip, port):
+        self.ip = ip
+        self.port = port
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def start(self):
+        self.socket.bind((self.ip, self.port))
+        print(f"Server gebunden an {self.ip}:{self.port}")
+
+    def listen(self):
+        self.socket.listen()
+        print("Server wartet auf Verbindung")
+        
+    def accept_connection(self):
+            client_socket, client_address = self.socket.accept()
+            print(f"Verbindung erfolgreich hergestellt mit {client_address}")
+
+    def recv_data(self, client_socket):
+        alleDaten = b''
+        while True:
+            daten = client_socket.recv(1024)
+            alleDaten += daten
+            if not daten:
+                break
+        return alleDaten
+    
+    def close(self):
+        self.socket.close()
+        print("Server geschlossen")
+    
