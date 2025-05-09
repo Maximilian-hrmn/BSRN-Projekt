81e7a52121d99147b43e78fc957e6fa9348da520
Sim0n362 <simongoering0306@gmail.com>
2025-05-09 11:15:45 +0200

    Exception abfangen für die TOML-Datei

--- main.py
+++ main.py
@@ -3,8 +3,20 @@ import socket
 
 def main():
 	
-    # TOML-Datei wird geladen und eingebetet
-    config = toml.load("config.toml")
+    # TOML-Datei wird geladen und eingebetet und mit try-catch abgefangen
+        try:
+            config = toml.load("config.toml")
+            print("Konfigurationsdatei geladen...")
+        
+        except FileNotFoundError: 
+            print("Konfigurationsdatei nicht gefunden.")
+            return  
+        
+        except toml.TomlDecodeError:
+            print("Fehler beim Dekodieren der Konfigurationsdatei.")
+            return
+        
+        
 
-    
+
 	
