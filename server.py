91c9fc9b46040aced818912a08d80b16001573da
Maximilian-hrmn <Maxi.herman@t-online.de>
2025-05-09 20:36:12 +0200

    server.py

--- server.py
+++ server.py
@@ -1,14 +1,14 @@
 import socket
 import threading
 
-#Konstruktor eines Servers
+#Konstruktor eines Servers, er braucht dafür eine IP-Adresse und einen Port
 class Server:
     def __init__(self, ip, port):
         self.ip = ip
         self.port = port
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-#Methode zum starten des Servers
+#Methode zum starten des Servers. Danach "lauscht" der Server nach Verbindungen
     def start(self):
         self.socket.bind((self.ip, self.port))
         self.socket.listen(5)
@@ -43,12 +43,13 @@ class Server:
                 break
             alleDaten += daten
         return alleDaten
-
+    
+#Schließt den Server
     def close(self):
         self.socket.close()
         print("Server geschlossen")
 
-# UDP-Responder fuer Discovery
+#UDP-Responder fuer Discovery
 def start_discovery_responder(listen_port=5000):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.bind(('', listen_port)) 
