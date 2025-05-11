4ec0823360a9e59b5540972d6a1ca03263dffeef
Maximilian-hrmn <Maxi.herman@t-online.de>
2025-05-11 22:33:39 +0200

    ui test

--- GUI.py
+++ GUI.py
@@ -1,24 +1,42 @@
-import tkinter as tk
-from tkinter import Tk, Label, Button, StringVar, Entry, messagebox
+import PyQt5
+from PyQt5 import QtCore, QtGui, QtWidgets
 
-#Fenster erstellen 
-fenster = Tk()
-fenster.title("Chatbox")
-fenster.configure(bg="white")
-fensterBreite = fenster.winfo_screenwidth()
-fensterHoehe = fenster.winfo_screenheight()
-fenster.geometry(f"{fensterBreite}x{fensterHoehe}+0+0")
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(789, 601)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.scrollArea = QtWidgets.QScrollArea(self.centralwidget)
+        self.scrollArea.setGeometry(QtCore.QRect(10, 20, 491, 471))
+        self.scrollArea.setWidgetResizable(True)
+        self.scrollArea.setObjectName("scrollArea")
+        self.scrollAreaWidgetContents = QtWidgets.QWidget()
+        self.scrollAreaWidgetContents.setGeometry(QtCore.QRect(0, 0, 489, 469))
+        self.scrollAreaWidgetContents.setObjectName("scrollAreaWidgetContents")
+        self.scrollArea.setWidget(self.scrollAreaWidgetContents)
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(400, 510, 101, 41))
+        self.pushButton.setObjectName("pushButton")
+        self.textEdit = QtWidgets.QTextEdit(self.centralwidget)
+        self.textEdit.setGeometry(QtCore.QRect(10, 510, 381, 41))
+        self.textEdit.setObjectName("textEdit")
+        self.listWidget = QtWidgets.QListWidget(self.centralwidget)
+        self.listWidget.setGeometry(QtCore.QRect(520, 20, 256, 531))
+        self.listWidget.setObjectName("listWidget")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 789, 24))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
 
-#Eingabe und Button Frame erstellen
-frameUnten = tk.Frame(fenster, bg="white")
-frameUnten.pack(side="bottom", fill="x", padx=10, pady=5)
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
-#Senden Button erstellen
-sendenButton =  Button(frameUnten, text="Senden")
-sendenButton.grid(row=0, column=1, padx=10, pady=10)
-
-#Eingabefeld erstellen
-eingabe = tk.Entry(frameUnten, width=100)  
-eingabe.grid(row=0, column=0, padx=5, pady=5, sticky="ew")
-
-fenster.mainloop()
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton.setText(_translate("MainWindow", "Send"))
