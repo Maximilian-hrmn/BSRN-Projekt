71aef320bd448bc887473c301b4bfb86ae3fe31e
Jan <jan.fabiunke@web.de>
2025-05-11 17:12:23 +0200

    main.py

--- main.py
+++ main.py
@@ -3,23 +3,21 @@ import socket
 from discovery_service import discover_peers
 
 def main():
-	
     # TOML-Datei wird geladen und eingebetet und mit try-catch abgefangen
-        try:
-            config = toml.load("config.toml")
-            print("Konfigurationsdatei geladen.")
+    try:
+        config = toml.load("config.toml")
+        print("Konfigurationsdatei geladen.")
         
-        except FileNotFoundError: 
-            print("Konfigurationsdatei nicht gefunden.")
-            return  
+    except FileNotFoundError: 
+        print("Konfigurationsdatei nicht gefunden.")
+        return  
         
-        except toml.TomlDecodeError:
-            print("Fehler beim Dekodieren der Konfigurationsdatei.")
-            return
+    except toml.TomlDecodeError:
+        print("Fehler beim Dekodieren der Konfigurationsdatei.")
+        return
         
-        discover_peers(config['discovery']['timeout'], config['discovery']['port'])
-        # Hier wird die Konfiguration verwendet
+    discover_peers(config['discovery']['timeout'], config['discovery']['port'])
+    # Hier wird die Konfiguration verwendet
 
-
-
-	
+if __name__ == "__main__":
+    main()
