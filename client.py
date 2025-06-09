94d38e92e85a7504100a3d862fda3d6b4560439d
Maximilian-hrmn <Maxi.herman@t-online.de>
2025-06-09 12:44:08 +0200

    vorbereitung doxygen client

--- client.py
+++ client.py
@@ -11,6 +11,7 @@ Client-Funktionen (Network-Sender):
 - Unicast (MSG/IMG) an target_host:target_port
 """
 
+# Funktion zum Senden einer JOIN-Nachricht an den Server
 def client_send_join(config):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
@@ -18,6 +19,7 @@ def client_send_join(config):
     sock.sendto(msg, (config['broadcast'], config['whoisport']))
     sock.close()
 
+# Funktion zum Senden einer WHO-Nachricht an den Server
 def client_send_who(config):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
@@ -25,6 +27,7 @@ def client_send_who(config):
     sock.sendto(msg, (config['broadcast'], config['whoisport']))
     sock.close()
 
+# Funktion zum Senden einer LEAVE-Nachricht an den Server
 def client_send_leave(config):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
@@ -32,12 +35,13 @@ def client_send_leave(config):
     sock.sendto(msg, (config['broadcast'], config['whoisport']))
     sock.close()
 
+#Funktion zum Senden einer MSG-Nachricht an einen bestimmten Host und Port
 def client_send_msg(target_host: str, target_port: int, from_handle: str, text: str):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     data = build_msg(from_handle, text)
     sock.sendto(data, (target_host, target_port))
     sock.close()
-
+#Funktion zum Senden eines Bildes an einen bestimmten Host und Port
 def client_send_img(target_host: str, target_port: int, from_handle: str, img_path: str):
     if not os.path.isfile(img_path):
         return False
