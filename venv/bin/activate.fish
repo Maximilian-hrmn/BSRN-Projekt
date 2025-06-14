90134178cfed47a01290a59916ca3b4b7fa73fc0
Maximilian-hrmn <Maxi.herman@t-online.de>
2025-06-14 14:21:41 +0200

    fix

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/maxi/BSRN-Projekt/BSRN-Projekt/venv
+set -gx VIRTUAL_ENV /home/maxi/BSRN-Projekt/BSRN-Projekt-1/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
